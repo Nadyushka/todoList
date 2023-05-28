f7302bb75225f9a4f435c9e22c3200b8d36bab21
Nadyushka <12nadya11@gmail.com>
2023-05-28 14:45:40 +0300

    Updates

---
+++ precache-manifest.5535895d56b5449ffcd3d746775b122e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a3fbcb97f0025260eb89bd6f67c40d",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "19e0ce291f384c89445d",
+    "url": "/todoList/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todoList/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19e0ce291f384c89445d",
+    "url": "/todoList/static/js/main.2bdc9993.chunk.js"
+  },
+  {
+    "revision": "c39c464575b971d22f9a",
+    "url": "/todoList/static/js/runtime-main.67bcfed6.js"
+  }
+]);
